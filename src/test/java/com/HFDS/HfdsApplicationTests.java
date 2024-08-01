916af55920e39a8a85adca65c321bd60a71a8942
Akash Kumar <akash983530@gmail.com>
2024-08-02 00:59:46 +0530

    File_Structure

---
+++ src/test/java/com/HFDS/HfdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HFDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
